680dcf59b7474b5fe1554e82cf962d38fbfe3899
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-06-24 20:33:37 +0530

    added react-router-dom, learnt about routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>This is the homepage</h1>
+    </div>
+  );
+};
+
+const Starred = () => {
+  return (
+    <div>
+      <h1>This is the starred page</h1>
+    </div>
+  );
+};
+
+const Page404 = () => {
+  return (
+    <div>
+      <h1>404 Error</h1>
+      <p>Page not found</p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Homepage />} />
+      <Route exact path="/starred" element={<Starred />} />
+      <Route path="*" element={<Page404 />} /> {/* this is for a 404 page */}
+    </Routes>
   );
 };
 
