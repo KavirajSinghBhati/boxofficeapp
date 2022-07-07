0fbf251681f4021627383fdc1c5e63a414f6e796
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-07-07 19:58:51 +0530

    extracted logic out of Show.jsx

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
-import { useReducer, useEffect } from "react";
+import { useReducer, useEffect, useState } from "react";
+import { apiGet } from "./config";
 
 const showsReducer = (prevState, action) => {
   switch (action.type) {
@@ -28,6 +29,68 @@ const usePersistedReducer = (reducer, initialState, key) => {
 
 export function useShows(key = "shows") {
   return usePersistedReducer(showsReducer, [], key);
+}
+
+export function useLastQuery(key = "lastQuery") {
+  const [input, setInput] = useState(() => {
+    const persisted = sessionStorage.getItem(key);
+    return persisted ? JSON.parse(persisted) : "";
+  });
+
+  const setPersistedInput = newState => {
+    setInput(newState);
+    sessionStorage.setItem(key, JSON.stringify(newState));
+  };
+
+  return [input, setPersistedInput];
+}
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case "FETCH_SUCCESS": {
+      return { ...prevState, show: action.show, isLoading: false };
+    }
+    case "FETCH_FAILED": {
+      return { ...prevState, error: action.error, isLoading: false };
+    }
+    default:
+      return prevState;
+  }
+};
+
+export function useShow(showId) {
+  const [state, dispatch] = useReducer(reducer, {
+    show: null,
+    isLoading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    let isMounted = true; // when we switch b/w pages and data still loads, it removes err from console
+    apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
+      .then(result => {
+        if (isMounted) {
+          dispatch({
+            type: "FETCH_SUCCESS",
+            show: result,
+          });
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({
+            type: "FETCH_FAILED",
+            error: err.message,
+          });
+        }
+      });
+    return () => {
+      // cleanup function when component is unmounted
+      isMounted = false;
+    };
+  }, [showId]);
+
+  return state;
 }
 
 // useReducer takes a 3 argument, that returns our initial state
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useReducer } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import { apiGet } from "../misc/config";
 
 import ShowMainData from "../components/show/ShowMainData";
 import Cast from "../components/show/Cast";
@@ -8,59 +7,12 @@ import Seasons from "../components/show/Seasons";
 import Details from "../components/show/Details";
 
 import { ShowPageWrapper, InfoBlock } from "./Show.styled.js";
-
-const initialState = {
-  show: null,
-  isLoading: true,
-  error: null,
-};
-
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case "FETCH_SUCCESS": {
-      return { ...prevState, show: action.show, isLoading: false };
-    }
-    case "FETCH_FAILED": {
-      return { ...prevState, error: action.error, isLoading: false };
-    }
-    default:
-      return prevState;
-  }
-};
+import { useShow } from "../misc/custom-hooks";
 
 const Show = () => {
   const { id } = useParams();
 
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  useEffect(() => {
-    let isMounted = true; // when we switch b/w pages and data still loads, it removes err from console
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then(result => {
-        if (isMounted) {
-          dispatch({
-            type: "FETCH_SUCCESS",
-            show: result,
-          });
-        }
-      })
-      .catch(err => {
-        if (isMounted) {
-          dispatch({
-            type: "FETCH_FAILED",
-            error: err.message,
-          });
-        }
-      });
-
-    return () => {
-      // cleanup function when component is unmounted
-      isMounted = false;
-    };
-  }, [id]);
+  const { show, isLoading, error } = useShow(id);
 
   if (isLoading) {
     return <div>Data is being loaded</div>;
