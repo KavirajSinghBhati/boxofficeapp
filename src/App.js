c29447d29c98e004bc9657704f556eebd30637ba
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-07-07 12:35:05 +0530

    edit show page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
+import Show from "./pages/Show";
 import Page404 from "./pages/Page404";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/starred" element={<Starred />} />
+        <Route exact path="/show/:id" element={<Show />} />
         <Route path="*" element={<Page404 />} /> {/* this is for a 404 page */}
       </Routes>
     </div>
---
+++ src/pages/Show.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { apiGet } from "../misc/config";
+
+const Show = () => {
+  const { id } = useParams();
+
+  const [show, setShow] = useState(null);
+
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(result =>
+      setShow(result)
+    );
+  }, [id]);
+
+  console.log(show);
+
+  return (
+    <div>
+      <h1>Show page</h1>
+    </div>
+  );
+};
+
+export default Show;
